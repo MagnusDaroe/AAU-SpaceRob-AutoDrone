81c0a3480c7c410fa9d4140266ae6c22ce75285f
Magnusdaroe <pt05wz@student.aau.dk>
2024-02-28 09:58:38 +0100

    Update FCController.py
    Updated connection to drone

--- Drone/FCController.py
+++ Drone/FCController.py
@@ -2,39 +2,30 @@ import subprocess
 import asyncio
 from mavsdk import System
 from serial.tools.list_ports import comports
-import serial
+import time
 
-ser = serial.Serial()
-
-def find_serial_port():
-    ports = list(comports())
-    for p in ports:
-        if 'Seriel USB-enhed' in p.description:
-            port = "serial://{}:57600".format(p.device)
-            return port
-    # If no port is found
-    print("No serial port found")
-    return None
+ServerAdress = 'C:/MagnusProjekter/mavsdk-windows-x64-release/bin/mavsdk_server_bin' # Adjust the path as needed
+port = '50051'  # Adjust the port as needed
+USB_search_string = 'Seriel USB-enhed'  # Adjust the search string as needed
 
 def connect_to_drone():
-    # Call the function to find the serial port
-    Drone_port = find_serial_port()
-    if Drone_port is not None:
-        print("Drone device found at port:", Drone_port)
-    elif Drone_port is None:
-        print("No serial port found")
-        print("Please connect the drone to the computer")
-        # Exit the program
-        exit()
+    while True:
+        try:
+            port = next("serial://{}:57600".format(p.device) for p in comports() if USB_search_string in p.description)
+            print("Drone device found at port:", port)
+            return port
+        except StopIteration:
+            print("No serial port found. Trying again in 5 seconds...")
+            time.sleep(5)
 
 async def main():
     # Start mavsdk_server and connect to the drone
-    server_process = subprocess.Popen(['C:/MagnusProjekter/mavsdk-windows-x64-release/bin/mavsdk_server_bin', '-p', '50051'])  # Adjust the path and port as needed
-    connect_to_drone()
+    server_process = subprocess.Popen([ServerAdress, '-p', port])  # Adjust the path and port as needed
+    drone_port = connect_to_drone()
 
     # Connect to the Pixhawk via USB
-    drone = System(mavsdk_server_address=server_process, port=50051)
-    await drone.connect(system_address="serial:///tty/COM3\[:57600\]")
+    drone = System(mavsdk_server_address=f"localhost:{port}")
+    await drone.connect(system_address=drone_port)
     print("Connected to Pixhawk via USB")
     # Your code logic here...
 
