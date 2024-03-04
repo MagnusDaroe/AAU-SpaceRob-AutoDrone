8b4cdfa71faa0b5f37da8c8165d04406bba97ee7
Copetags67 <simondonslund@live.dk>
2024-03-04 08:29:27 +0100

    Update Try.py

---
+++ Drone/Try/Try.py
@@ -0,0 +1,9 @@
+from pymavlink import mavutil
+
+# Start a connection listening on a UDP port
+the_connection = mavutil.mavlink_connection('udpin:localhost:14551')
+
+# Wait for the first heartbeat
+#   This sets the system and component ID of remote system for the link
+the_connection.wait_heartbeat()
+print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
