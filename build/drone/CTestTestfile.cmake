f9d5bc977c3cc0e618528aa33c1879957d6db032
Magnusdaroe <pt05wz@student.aau.dk>
2024-03-25 09:43:21 +0100

    Gitignore change

--- build/drone/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetson/P4_DroneProject/drone
-# Build directory: /home/jetson/P4_DroneProject/build/drone
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
