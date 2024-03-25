e234aba28c34db3870cdb8ca3eef3271dee82287
Magnusdaroe <pt05wz@student.aau.dk>
2024-03-25 10:32:36 +0100

    Update manual_control.py
    Tkinter istead of keyboard

--- drone/scripts/manual_control.py
+++ drone/scripts/manual_control.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python3
-
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-import pygame
+import tkinter as tk
 
 class ManualControlNode(Node):
     def __init__(self):
@@ -17,40 +15,39 @@ class ManualControlNode(Node):
         self.control_msg.angular.z = angular_z
         self.publisher_.publish(self.control_msg)
 
+def on_key_press(event):
+    key = event.char
+    if key in CONTROL_DICT:
+        control[CONTROL_DICT[key][1]] += CONTROL_DICT[key][0]
+        print(control)
+        # Publish control command via ROS 2
+        node.send_control_command(control[0], control[1], control[3])
+    elif key == 'q':
+        # Signal to exit the loop
+        root.destroy()
 
 def Manual_Control():
+    global root, node, CONTROL_DICT, control
     rclpy.init()
     node = ManualControlNode()
 
-    # Initialize Pygame
-    pygame.init()
+    root = tk.Tk()
+    root.title("Manual Control")
 
     # List with values of the keys: ('key', [value increment, index of the control list])
-    CONTROL_DICT = {'w': [1, 0], 's': [-1, 0], 'a': [1, 1], 'd': [-1, 1], 'space': [1, 2], 'shift': [-1, 2], 'q':[1,3], 'e':[-1,3]}
+    CONTROL_DICT = {'w': [1, 0], 's': [-1, 0], 'a': [1, 1], 'd': [-1, 1], ' ': [1, 2], 'Shift_L': [-1, 2], 'q': [1, 3], 'e': [-1, 3]}
     control = [0, 0, 0, 0]
 
-    try:
-        # Pygame loop
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                elif event.type == pygame.KEYDOWN:
-                    key = pygame.key.name(event.key)
-                    if key in CONTROL_DICT:
-                        control[CONTROL_DICT[key][1]] += CONTROL_DICT[key][0]
-                        print(control)
-                        node.send_control_command(control[0], control[1], control[3])
-                    elif key == 'escape':
-                        running = False
+    # Bind key press event to the function
+    root.bind('<KeyPress>', on_key_press)
 
-    finally:
-        # Shutdown ROS 2 node
-        node.destroy_node()
-        rclpy.shutdown()
-        pygame.quit()
+    root.mainloop()
 
+    print("escaped")
+
+    # Shutdown ROS 2 node
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     Manual_Control()
