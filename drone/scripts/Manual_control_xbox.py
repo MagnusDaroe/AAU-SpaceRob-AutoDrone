da6a63d93a2aecfc97735ea2835a7000aae14b78
MagnusDaroe <116544128+MagnusDaroe@users.noreply.github.com>
2024-03-25 14:08:38 +0100

    Joystick now maps correctly

--- drone/scripts/Manual_control_xbox.py
+++ drone/scripts/Manual_control_xbox.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import Twist
 import threading
 from inputs import get_gamepad
 import math
+import numpy as np
 
 class XboxController:
     MAX_JOY_VAL = math.pow(2, 15)
@@ -54,10 +55,18 @@ def control_loop(node):
     while True:
         left_x, left_y, right_x, right_y = node.controller.read()
         twist = Twist()
-        twist.linear.x = right_x  # linear x from right joystick
-        twist.linear.y = right_y  # linear y from right joystick
-        twist.linear.z = left_x   # Throttle z from right joystick
-        twist.angular.z = left_y  # yaw from left joystick's Y-axis
+
+        # If any of the values are less than 0.1 of the absolute value, set them to 0
+        left_x = left_x if abs(left_x) >= 0.1 else 0
+        left_y = left_y if abs(left_y) >= 0.1 else 0
+        right_x = right_x if abs(right_x) >= 0.1 else 0
+        right_y = right_y if abs(right_y) >= 0.1 else 0
+
+        twist.linear.x = float(np.interp(right_x, (-1, 1), (-1000, 1000)))  # linear x from right joystick
+        twist.linear.y = float(np.interp(right_y, (-1, 1), (-1000, 1000)))  # linear y from right joystick
+        twist.linear.z = float(np.interp(left_y, (-1, 1), (-1000, 1000)))   # Throttle z from right joystick
+        twist.angular.z = float(np.interp(left_x, (-1, 1), (-1000, 1000)))  # yaw from left joystick's Y-axis
+        
         node.send_control_command(twist)
 
 def main(args=None):
