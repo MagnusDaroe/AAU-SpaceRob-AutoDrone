5eaf9be2592156f8c268b605b4251ed32b75dbc5
MNejmann <Mikkel@nejmann.net>
2024-04-02 08:50:17 +0200

    Client code

--- fuck/client.py
+++ fuck/client.py
@@ -1,6 +1,8 @@
 import socket
+import socket
+import time
 
-HOST = "172.18.64.1"  # Replace 'ubuntu_ip' with the IP address of your Ubuntu machine
+HOST = "192.168.60.251"  # Replace 'ubuntu_ip' with the IP address of your Ubuntu machine
 PORT = 12345        # The port on which the server is listening
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
@@ -8,7 +10,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
     print("Connected to server")
 
     while True:
-        data = input("Enter data to send (or 'exit' to quit): ")
+        #data = input("Enter data to send (or 'exit' to quit): ")
+        data="2\n"
         if data.lower() == 'exit':
             break
         client_socket.sendall(data.encode())
