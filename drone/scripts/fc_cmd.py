629f633fb67d5617405f7791559eeb18c9924ba3
MagnusDaroe <116544128+MagnusDaroe@users.noreply.github.com>
2024-05-03 13:18:59 +0200

    test

--- drone/scripts/fc_cmd.py
+++ drone/scripts/fc_cmd.py
@@ -392,6 +392,7 @@ class FC_Commander(Node):
                 
                 # Arm the drone again
                 if not self.test_mode:
+                    self.get_logger().info(self.fc_command.cmd_arm, self.fc_command.cmd_estop)
                     self.drone_arm()
     
     def flight_mode(self):
