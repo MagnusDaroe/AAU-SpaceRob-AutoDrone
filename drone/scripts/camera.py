d0f19825a534a846b6d003e329bf5beda43bc9b9
MagnusDaroe <116544128+MagnusDaroe@users.noreply.github.com>
2024-05-08 13:50:41 +0200

    fixed typo

--- drone/scripts/camera.py
+++ drone/scripts/camera.py
@@ -65,12 +65,12 @@ class T265(Node):
 
         # Transformation from FC to backside center of T265:
         self.R_FC_backside=np.array([[-1,0,0],[0,-1,0],[0,0,1]])
-        self.R_FC_backside=np.array([[-1,0,0,0],[0,-1,0,0],[0,0,1,0],[15.3223,-8.66070,0,1]]) #cm
+        self.T_FC_backside=np.array([[-1,0,0,0],[0,-1,0,0],[0,0,1,0],[15.3223,-8.66070,0,1]]) #cm
 
 
         # Transformation from FC to T265 pose frame:
         self.R_FC_cam=self.R_FC_backside@self.R_backside_local@self.R_local_cam
-        self.T_FC_cam=self.R_FC_backside@self.T_backside_local@self.T_local_cam
+        self.T_FC_cam=self.T_FC_backside@self.T_backside_local@self.T_local_cam
 
         # Transformation from global to T265 pose frame:
         self.R_global_cam=self.R_FC_cam
@@ -231,6 +231,7 @@ class T265(Node):
             self.publisher_.publish(msg)
         else: 
             self.get_logger().warning('No global frame data available')
+            time.sleep(0.2)
 
 
 
