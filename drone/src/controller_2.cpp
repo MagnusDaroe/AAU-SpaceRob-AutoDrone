6c23f23fbbc3a424692f0f44781975d3ad689850
Emil <133101975+L0bsterman@users.noreply.github.com>
2024-05-09 14:02:04 +0200

    Update controller_2.cpp

--- drone/src/controller_2.cpp
+++ drone/src/controller_2.cpp
@@ -53,9 +53,9 @@ private:
     // cook
     const static int array_size = 2;
     float x_ref_list[array_size] = {1200, 500};
-    // float y_ref_list[array_size] = {1000, -700};
-    // float z_ref_list[array_size] = {500, 1000}; 
-    // float yaw_ref_list[array_size] = {0, 90};
+    float y_ref_list[array_size] = {1000, -700};
+    float z_ref_list[array_size] = {500, 1000}; 
+    float yaw_ref_list[array_size] = {0, 90};
 
     float x_ref;
     float y_ref;
