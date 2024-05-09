e8663d1473206bfba6f1fa8981177b4107434aca
MagnusDaroe <116544128+MagnusDaroe@users.noreply.github.com>
2024-05-09 18:21:59 +0200

    ??

--- drone/launch/fc_cmd_launch.py
+++ drone/launch/fc_cmd_launch.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import launch
 from launch_ros.actions import Node
 
@@ -7,24 +9,12 @@ def generate_launch_description():
             package='drone',
             executable='fc_cmd.py',
             name='FCCommander',
-            parameters=[
-                {'test_type': 'pitch'},
-                {'test_pitch_value': '200'},  # Set the test pitch value
-                {'test_roll_value': '200'},   # Set the test roll value
-                {'test_yaw_value': '200'},    # Set the test yaw value
-                {'test_thrust_value': '300'}, # Set the test thrust value
-            ],
             output='screen', # to see the output of the node
         ),
         Node(
             package='drone',
             executable='Controller_node',
             output='screen',  # Direct output to screen (terminal)
-            parameters=[{
-                'ros__parameters': {
-                    'rosout__console__format': '[${severity}] [${time}] [${node}] [${logger}] [${message}]'
-                }
-            }],
         ),
     ])
 
