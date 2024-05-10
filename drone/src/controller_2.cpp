35aa31207f418fb58759b62ba2310aaeb57ea7eb
Emil <133101975+L0bsterman@users.noreply.github.com>
2024-05-10 09:41:46 +0200

    Comments

--- drone/src/controller_2.cpp
+++ drone/src/controller_2.cpp
@@ -22,105 +22,120 @@ public:
     }
 
 private:
-    // Variables
+    // Variables to hold the output error after converting from global to local coordinates
     float local_error_x;
     float local_error_y;
 
-    // XY controller
-    // -Ouputs
+    // XY controller function variables
+    // Outputs
     float regulator_pitch_value;
     float regulator_roll_value;
 
-    // -Inputs
+    // Variables to store previous error
     float prev_x_error = 0;
     float prev_y_error = 0;
 
     // Z controller
-    // -Outputs
+    // Output
     float regulator_altitude_value;
 
-    // -Inputs
+    // Previous error value
     float prev_z_error = 0;
 
     // Yaw controller
-    // -Outputs
+    // Outputs
     float regulator_yaw_value;
 
-    // timer shit
+    // Variable to check if waypoint is reached and new data is requested
     bool data_request = true;
 
 
-    // cook
-    const static int array_size = 2;
+    // Defining waypoints
+    const static int array_size = 2;            // size of array
+
     float x_ref_list[array_size] = {1200, 500};
     float y_ref_list[array_size] = {1000, -700};
     float z_ref_list[array_size] = {500, 1000}; 
     float yaw_ref_list[array_size] = {0, 90};
 
+    int array_counter = 0;                     // counter for array
+
+    // Variables that hold individual reference points while running the controller
     float x_ref;
     float y_ref;
     float z_ref;
     float yaw_ref;
 
+    // Sample time of all controllers
     float sample_time = 0.01;
 
-    int array_counter = 0;
-
-
+    // Defines time variables
     std::chrono::system_clock::time_point time_start;
     std::chrono::system_clock::time_point time_stop;
 
+    // Subscribers and publishers
     rclcpp::Subscription<drone::msg::ViconData>::SharedPtr Data_subscription_;
     rclcpp::Publisher<drone::msg::DroneCommand>::SharedPtr Control_publisher_;
 
     //Controller functions
-    void DataCallback(const drone::msg::ViconData::SharedPtr msg) // skal ændres hvis vi vil køre på vicon data
+    void DataCallback(const drone::msg::ViconData::SharedPtr msg) // skal ændres hvis vi vil køre på kamera data data
     { 
         // Check if data is requested. Reset data and timer if so
         if (data_request == true)
         {
+            // Next waypoint is saved in seperate variable
             x_ref = x_ref_list[array_counter];
             y_ref = y_ref_list[array_counter];
             z_ref = z_ref_list[array_counter]; 
             yaw_ref = yaw_ref_list[array_counter];
 
-            time_start = std::chrono::system_clock::now();
-            data_request = false;
+            time_start = std::chrono::system_clock::now(); // Timer is reset
+            data_request = false;                          // Reset data request
 
-            if(array_counter < array_size-2){ // ÆNDRE TILBAGE TIL 1
+            // Only increment if there are more waypoints
+            if(array_counter < array_size-1){
                 array_counter++;
             }
         }
+        // Check time now and calculate time duration
         time_stop = std::chrono::system_clock::now();
         auto time_duration = std::chrono::duration_cast<std::chrono::milliseconds>(time_stop - time_start).count();
         std::cout << "Time duration: " << time_duration << std::endl;
 
-        // kører xy controller
-        float x_ref_signal = ref_signal(time_duration/1000, x_ref, 2); // time duration omregnet til sekunder
-        float y_ref_signal = ref_signal(time_duration/1000, y_ref, 2); // time duration omregnet til sekunder
+        // XY controller
+        // Generates a reference signal according to exponential function
+        // Signal function requires current time, final reference signal, and time constant
+        float x_ref_signal = ref_signal(time_duration/1000, x_ref, 2); // time duration is converted to seconds
+        float y_ref_signal = ref_signal(time_duration/1000, y_ref, 2); // time duration is converted to seconds
+        // recieves the reference signal and the current position and calculates the local error
         globalErrorToLocalError(x_ref_signal, y_ref_signal, msg->vicon_x, msg->vicon_y, msg->vicon_yaw);
+        // recieves the local error and calculates controller values for roll and pitch
         XY_controller(local_error_x, local_error_y);
 
         // Z controller
-        float z_ref_signal = ref_signal(time_duration/1000, z_ref, 1); // time duration omregnet til sekunder
-        Z_controller(z_ref_signal, msg->vicon_z); // note vicon ups
+        // Generates reference signal
+        float z_ref_signal = ref_signal(time_duration/1000, z_ref, 1); // time duration is converted to seconds
+        // Generates controller value for altitude
+        Z_controller(z_ref_signal, msg->vicon_z); // Note: skal ændres hvis kamera skal bruges
 
         // Yaw controller
-        float yaw_signal = ref_signal(time_duration/1000, yaw_ref, 1); // time duration omregnet til sekunder
+        // Generates reference signal
+        float yaw_signal = ref_signal(time_duration/1000, yaw_ref, 1); // time duration is converted to seconds
+        // Generates controller value for yaw
         yaw_controller(yaw_signal, msg->vicon_yaw);
 
-        // How close is it to the reference point
+        // Shitty way to calculate the distance to the point but square roots and shit aint worth it
         float total_error = abs((msg->vicon_x + msg->vicon_y + msg->vicon_z) - (x_ref + y_ref + z_ref));
 
         // Check if error is under threshold to request new data
-        if (total_error < 0){   // SKAL SÆTTES TIL AFSTAND LIMIT
-            data_request = true;
+        if (total_error < 0){   // SKAL SÆTTES TIL AFSTAND LIMIT FØR SKIDTE VIRKER
+            data_request = true;    // Reset data request if close to waypoint
         }
         else{
-            data_request = false;
+            data_request = false;   // Still false if not close
         }
 
-        // Publish regulated pitch, roll, and thrust values
+        // Publish regulated pitch, roll, thrust, and yaw values
         auto control_msg = drone::msg::DroneCommand();
         control_msg.cmd_auto_roll = regulator_roll_value;
         control_msg.cmd_auto_pitch = regulator_pitch_value;
@@ -166,38 +181,41 @@ private:
     }
 
     void yaw_controller(float yaw_ref, float yaw_mes)
+    // P controller for yaw position
     {
-        float Kp_yaw = 15;
-        float saturation_value = 900;
+        float Kp_yaw = 15;              // Proportional gain
+        float saturation_value = 900;   // Max and min value allowed to be sent to the drone
 
-        float yaw_error = yaw_ref - yaw_mes;
+        float yaw_error = yaw_ref - yaw_mes;  // Error between reference and measurement
 
-        float yaw_value = Kp_yaw * yaw_error;
+        float yaw_value = Kp_yaw * yaw_error; // P regulaed value
 
-        regulator_yaw_value = saturation(yaw_value, saturation_value);
+        regulator_yaw_value = saturation(yaw_value, saturation_value);  // Saturate value
     }
 
     void Z_controller(float z_ref, float z_mes)
+    // PD controller for altitude
     {
-        float Kp_altitude = 0.01;
-        float Kd_altitude = 10;
-        float saturation_value = 900;
+        float Kp_altitude = 0.01;       // Proportional gain
+        float Kd_altitude = 10;         // Derivative gain
+        float saturation_value = 900;   // Max and min value allowed to be sent to the drone
 
-        float z_error = z_ref - z_mes;
+        float z_error = z_ref - z_mes;  // Error between reference and measurement
 
-        float altitude_value = Kd_altitude*((z_error - prev_z_error)/sample_time)+Kp_altitude*z_error;
+        float altitude_value = Kd_altitude*((z_error - prev_z_error)/sample_time)+Kp_altitude*z_error; // PD regulated value
 
-        regulator_altitude_value = saturation(altitude_value, saturation_value, 1);
+        regulator_altitude_value = saturation(altitude_value, saturation_value, 1); // Saturate value
 
-        prev_z_error = z_error;
+        prev_z_error = z_error; // Update previous error
     }
 
     void XY_controller(float local_x_error, float local_y_error)
+    // PD controller for x and y
     {
-        // Define PD controller parameters
-        float Kp_pitch = 0.002; // originalt 0.002
+        // PD controller gains
+        float Kp_pitch = 0.002; 
         float Kd_pitch = 0.7;
-        float Kp_roll = 0.002;  // originalt 0.002
+        float Kp_roll = 0.002;
         float Kd_roll = 0.7;
 
         // Max allowed value (1000 is max max, but we aint chill like that)
@@ -207,28 +225,29 @@ private:
         float pitch_value = (Kd_pitch*(local_x_error-prev_x_error)/sample_time)+local_x_error*(Kp_pitch);
         float roll_value = (Kd_roll*(local_y_error-prev_y_error)/sample_time)+local_y_error*(Kp_roll);
 
-
+        // Saturate values
         regulator_pitch_value = saturation(pitch_value, saturation_value);
         regulator_roll_value = saturation(roll_value, saturation_value);
 
+        // Update previous errors
         prev_x_error = local_x_error;
         prev_y_error = local_y_error;
     } 
 
     float saturation(float value, float max_value, int only_positive = 0)
+    // Saturate value to + or - max_value
     {
-        // Saturate value to + or - max_value
         if (value > max_value)       // Saturate to max value
         {
             value = max_value;
         }
         else if (value < -max_value) // Saturate to minimum
         {
-            if (only_positive == 0){
+            if (only_positive == 0){ // Check if only positive values are allowed
                 value = -max_value;
             }
             else{
-                value = 1; // Smallest allowed value when only saturating to positive numbers
+                value = 1;           // Smallest allowed value when only saturating to positive numbers
             }
         }
         else                         // Do nothing
@@ -239,6 +258,7 @@ private:
     }
     
     float ref_signal(float t, float ref, float delay)
+    // Generates a reference signal according to exponential function
     {
         float signal = ref*(1-exp(-t/delay));
         return signal;
