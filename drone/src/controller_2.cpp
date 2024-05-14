59a2744baf3806cef1ac7a39bcc784b0ea0e6122
Copetags67 <simondonslund@live.dk>
2024-05-14 10:20:24 +0200

    Update controller_2.cpp

--- drone/src/controller_2.cpp
+++ drone/src/controller_2.cpp
@@ -167,8 +167,8 @@ private:
 
         // Publish regulated pitch, roll, thrust, and yaw values
         auto control_msg = drone::msg::DroneCommand();
-        control_msg.cmd_auto_roll = -regulator_roll_value; //(minus)Because of Henriks ligninger /Kamera
-        control_msg.cmd_auto_pitch = regulator_pitch_value;
+        control_msg.cmd_auto_roll = 0;//-regulator_roll_value; //(minus)Because of Henriks ligninger /Kamera
+        control_msg.cmd_auto_pitch = 0;//regulator_pitch_value;
         control_msg.cmd_auto_thrust = regulator_altitude_value;
         std::cout<<"altitude val: "<< regulator_altitude_value << std::endl;
         control_msg.cmd_auto_yaw = 0;//regulator_yaw_value;
