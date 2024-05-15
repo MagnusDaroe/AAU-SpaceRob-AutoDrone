5e9703daf40c8068856e45a93db016ea876fccbd
MagnusDaroe <116544128+MagnusDaroe@users.noreply.github.com>
2024-05-15 19:25:49 +0200

    Update camera.py

--- drone/scripts/camera.py
+++ drone/scripts/camera.py
@@ -292,14 +292,14 @@ class T265(Node):
                 #self.get_logger().info(f"diff_x: {round(self.diff_x,2)}, diff_y: {round(self.diff_y,2)}, diff_z: {round(self.diff_z,2)}")
 
                 #self.get_logger().info(f"vicon pose: x: {round(self.vicon_x,2)}, y: {round(self.vicon_y,2)}, z: {round(self.vicon_z,2)}")
-                ##self.get_logger().info(f"Global pose: x: {round(self.t_vec_global_FC[0],2)}, y: {round(self.t_vec_global_FC[1],2)}, z: {round(self.t_vec_global_FC[2],2)}")
+                self.get_logger().info(f"Global pose: x: {round(self.t_vec_global_FC[0],2)}, y: {round(self.t_vec_global_FC[1],2)}, z: {round(self.t_vec_global_FC[2],2)}")
                 self.R_to_euler_angles()
 
                 #self.get_logger().info(f"Euler angles xyz: {self.euler_xyz}")
-                ##self.get_logger().info(f"Euler angles xyz deg: x: {round(math.degrees(self.euler_xyz[0]),2)}, y: {round(math.degrees(self.euler_xyz[1]),2)}, z: {round(math.degrees(self.euler_xyz[2]),2)}")
+                self.get_logger().info(f"Euler angles xyz deg: x: {round(math.degrees(self.euler_xyz[0]),2)}, y: {round(math.degrees(self.euler_xyz[1]),2)}, z: {round(math.degrees(self.euler_xyz[2]),2)}")
 
             msg = DroneControlData()
-            #msg.timestamp = time.time()
+            msg.timestamp = time.time()
             msg.camera_x = float(self.t_vec_global_FC[0]) # mm
             msg.camera_y = float(self.t_vec_global_FC[1]) # mm
             msg.camera_z = float(self.t_vec_global_FC[2]) # mm
