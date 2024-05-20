c6a22eb32535e973bcd5f1eb53d906ef1222d2b1
MagnusDaroe <116544128+MagnusDaroe@users.noreply.github.com>
2024-05-20 11:34:52 +0200

    Update fc_cmd.py

--- drone/scripts/fc_cmd.py
+++ drone/scripts/fc_cmd.py
@@ -526,7 +526,7 @@ class FC_Commander(Node):
             # Check if the command is new or if the timeout has expired
             self.current_time = self.get_time()
 
-            self.get_logger().info(f"first loop: {self.fc_command.identifier == 0 and self.fc_command.cmd_mode == 0 and (self.previous_timestamp_manual != self.timestamp_manual or self.current_time - self.last_command_time_manual <= self.TIMEOUT)}, second loop : {self.fc_command.identifier == 1 and self.fc_command.cmd_mode == 1 and (self.previous_timestamp_auto != self.timestamp_auto or self.current_time - self.last_command_time_auto <= self.TIMEOUT)} ")
+            self.get_logger().info(f"first loop: {self.fc_command.identifier == 0 and (self.previous_timestamp_manual != self.timestamp_manual or self.current_time - self.last_command_time_manual <= self.TIMEOUT)}, second loop : {self.fc_command.identifier == 1 and (self.previous_timestamp_auto != self.timestamp_auto or self.current_time - self.last_command_time_auto <= self.TIMEOUT)} ")
                     
             if self.fc_command.identifier == 0 and (self.previous_timestamp_manual != self.timestamp_manual or self.current_time - self.last_command_time_manual <= self.TIMEOUT):
                 # Send the command to the flight controller
